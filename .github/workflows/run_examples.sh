d81f1bdbaf9cba10d776923d43dfcc62535906ff
dcluo <dc.luo@outlook.com>
2023-07-19 21:26:07 -0500

    fixed typos in run script

--- .github/workflows/run_examples.sh
+++ .github/workflows/run_examples.sh
@@ -4,8 +4,8 @@ export HIPPYLIB_PATH=$(pwd)/hippylib
 
 # Run poisson example
 cd examples/poisson
-python driver_poisson_mean_serial.py
-mpirun -n 2 python driver_poisson_mean_mpi.py
+python3 driver_poisson_mean_serial.py
+mpirun -n 2 python3 driver_poisson_mean_mpi.py
 python3 compare_results.py
 
 # Run hyperelasticity example 
