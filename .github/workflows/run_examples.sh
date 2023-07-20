b90f02874eff585f8978a3c3726c014ddd97ccaa
dcluo <dc.luo@outlook.com>
2023-07-19 21:59:56 -0500

    running just poisson test and mpi comparison

--- .github/workflows/run_examples.sh
+++ .github/workflows/run_examples.sh
@@ -4,13 +4,9 @@ export HIPPYLIB_PATH=$(pwd)/hippylib
 
 # Run poisson example
 cd examples/poisson
+
 python3 driver_poisson_mean_serial.py
+
 mpirun -n 2 python3 driver_poisson_mean_mpi.py
+
 python3 compare_results.py
-
-# Run hyperelasticity example 
-cd ../hyperelasticity
-python3 driver_hyperelasticity_deterministic.py
-
-cd ../semilinear_elliptic
-python3 driver_semilinear_cvar.py -N 4
