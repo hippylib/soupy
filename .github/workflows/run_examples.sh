3701b83f7182f07fa1d97709f85bdf348a8d765d
Dingcheng Luo <44854707+dc-luo@users.noreply.github.com>
2024-07-26 10:36:22 -0700

    Updating CI (#25)
    * trying different ci image
    
    * using python instead of python3
    
    * updated run scripts for using new image
    
    * adding conda

--- .github/workflows/run_examples.sh
+++ .github/workflows/run_examples.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
+set -ev 
+
+CONDA_ENV=dibasis
+PYTHON=python
 
 export HIPPYLIB_PATH=$(pwd)/hippylib
+export HIPPYFLOW_PATH=$(pwd)/hippyflow
+
+# This is for running MPI on root in the docker container 
+export OMPI_MCA_btl_vader_single_copy_mechanism=none
+export OMPI_ALLOW_RUN_AS_ROOT=1
+export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 
 # Run poisson example
 cd examples/poisson
 
-python3 driver_poisson_mean.py
+conda init 
+source ~/.bashrc
+conda activate $CONDA_ENV
 
-mpirun -n 2 python3 driver_poisson_mean.py
+$PYTHON driver_poisson_mean.py
+
+mpirun -n 2 --oversubscribe python driver_poisson_mean.py
 
--- .github/workflows/run_tests.sh
+++ .github/workflows/run_tests.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 set -ev 
 
-PYTHON=python3
-
+CONDA_ENV=dibasis
+PYTHON=python
 export HIPPYLIB_PATH=$(pwd)/hippylib
+
+# This is for running MPI on root in the docker container 
+export OMPI_MCA_btl_vader_single_copy_mechanism=none
+export OMPI_ALLOW_RUN_AS_ROOT=1
+export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 
 # Run the code
 cd soupy/test
+
+conda init 
+source ~/.bashrc
+conda activate $CONDA_ENV
+
 $PYTHON -m unittest discover -v
 $PYTHON -m unittest discover -v -p 'ptest_*'
 
-mpirun -n 4 $PYTHON ptest_meanVarRiskMeasureSAA.py
-mpirun -n 4 $PYTHON ptest_scipyCostWrapper.py
-mpirun -n 4 $PYTHON ptest_superquantileSAA.py
-mpirun -n 4 $PYTHON ptest_transformedMeanRiskMeasureSAA.py
+mpirun -n 2 --oversubscribe $PYTHON ptest_meanVarRiskMeasureSAA.py
+mpirun -n 2 --oversubscribe $PYTHON ptest_scipyCostWrapper.py
+mpirun -n 2 --oversubscribe $PYTHON ptest_superquantileSAA.py
+mpirun -n 2 --oversubscribe $PYTHON ptest_transformedMeanRiskMeasureSAA.py
+
